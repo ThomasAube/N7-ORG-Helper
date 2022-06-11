5a50d8ef92d5d32be69c3adacd520254da8adc65
Thomas <thomas.aube04@gmail.com>
2022-06-11 21:09:39 +0200

    add role to everyone

--- slashcommands/member/give-role.js
+++ slashcommands/member/give-role.js
@@ -2,14 +2,22 @@ const Discord = require("discord.js")
 
 const run = async (client, interaction) => {
     let role = interaction.options.getRole("role")
+    let everyone = interaction.options.getBoolean("everyone") || false
     let membersToUpdate = new Discord.Collection()
-    for(let i = 1; i <= 10; ++i) {
-        let member = interaction.options.getMember(`user-${i}`)
-        if(member) membersToUpdate.set(member.id, member)
+
+    await interaction.reply("Wait...")
+    if(everyone) {
+        membersToUpdate = await interaction.guild.members.fetch()
+    }
+    else {
+        for(let i = 1; i <= 10; ++i) {
+            let member = interaction.options.getMember(`user-${i}`)
+            if(member) membersToUpdate.set(member.id, member)
+        }
     }
     
     let updatedMembers = new Array()
-    if(membersToUpdate.size === 0) interaction.reply("There is no member to add role")
+    if(membersToUpdate.size === 0) interaction.editReply("There is no member to add role")
 
     const addingRole = new Promise((resolve, reject) => {
         membersToUpdate.forEach(m => {
@@ -22,7 +30,7 @@ const run = async (client, interaction) => {
 
     addingRole.then(() => {
         let answer = updatedMembers.map(m => `${m} have now the role ${role} !`).join('\n')
-        interaction.reply(answer)
+        interaction.editReply(answer)
     })
 }
 
@@ -38,10 +46,16 @@ module.exports = {
             required: true,
         },
         {
+            name: "everyone",
+            description: "Do you want to add this role for everyone ?",
+            type: "BOOLEAN",
+            required: false,
+        },
+        {
             name: "user-1",
             description: "A user you want to give the role to",
             type: "USER",
-            required: true,
+            required: false,
         },
         {
             name: "user-2",
